b15b65a3c1e1b00e6becf39182b3039c6fc84dd8
tsmarti1 <altimesmiles@yahoo.com>
2018-05-07 17:23:48 -0400

    d.b
    d.b incorporation of php start

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php header( 'Location: index.html' ) ; ?>
+<?php
+function ExtendedAddslash(&$params)
+{
+        foreach ($params as &$var) {
+            // check if $var is an array. If yes, it will start another ExtendedAddslash() function to loop to each key inside.
+            is_array($var) ? ExtendedAddslash($var) : $var=addslashes($var);
+        }
+}
+
+     // Initialize ExtendedAddslash() function for every $_POST variable
+    ExtendedAddslash($_POST);
+
+$form_idNum=$post['PersonId'];
+$form_name=$POST['FName'];
+$form_lastname=$POST['LName'];
+$form_email=$POST['Email'];
+$form_phone=$POST['Phone'];
+$form_message=$POST['Msg']
+
+$db_host = 'localhost';
+$db_username = 'root';
+$db_password = '';
+$db_name = 'submissions';
+mysql_connect( $db_host, $db_username, $db_password) or die(mysql_error());
+mysql_select_db($db_name);
+?>
